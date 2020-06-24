f6946dcf3aac63138243df9fe04563f1adc73462
Chris Tack <chris_tack@mac.com>
2020-06-24 09:55:27 -0700

    Fix typo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import classes from '/Header.module.css';
+import classes from './Header.module.css';
 import Logo from '../../elements/Logo/Logo';
 
 const header = (props) => {
